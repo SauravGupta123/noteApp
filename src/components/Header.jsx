f48e8768d44fec4e81506704f5229e4b55afa75d
Saurav <er.sauravgpt@gmail.com>
2023-10-01 22:59:36 +0530

    changes in the css and improved the layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
    <header>
-    <h3>Notes app</h3>
+    <h3>My Notes</h3>
    </header>
   )
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,12 +4,12 @@ import {getFirestore} from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGJ0RLNrifCf8n8aCWqjee-D51cfDuugw",
-  authDomain: "mynotesapp-eae5d.firebaseapp.com",
-  projectId: "mynotesapp-eae5d",
-  storageBucket: "mynotesapp-eae5d.appspot.com",
-  messagingSenderId: "100704111552",
-  appId: "1:100704111552:web:f7b51bc6a98290d23d1625"
+  apiKey: "AIzaSyBGNhSqcxayVRdfLhNwvrkRGhxAlQbfr_Y",
+  authDomain: "notesapp-4a21f.firebaseapp.com",
+  projectId: "notesapp-4a21f",
+  storageBucket: "notesapp-4a21f.appspot.com",
+  messagingSenderId: "922645059719",
+  appId: "1:922645059719:web:042129b35a77467a8ac022"
 };
 
 // Initialize Firebase
